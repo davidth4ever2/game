46170a538b8cdd97421c53225d074343f7c33111
David <davidth4ever2@gmail.com>
2018-11-23 05:35:00 -0500

    day after thanksgiving

---
+++ js/static.js
@@ -0,0 +1,24 @@
+document.custom 
+colors = ["white","silver",
+                "grey","red",
+                "maroon","yellow",
+                "olive","lime",
+                "green","aqua",
+                "teal","blue",
+                "navy","fuchsia",
+                "purple"
+            ]
+window.thems = [ {"Crime Doesn’t Pay" :{} },
+            {"Own Worst Enemy":{}},
+            {"Coming of Age":{}},
+            {"Death":{}},
+            {"Overcoming the Odds":{}},
+            {"Capitalism":{}},
+            {"Technology":{}},
+            {"Love conquers all":{}},
+            {"Religion":{}},
+            {"Humanity vs. nature":{}},
+            {"The individual vs. society":{}},
+            {"Family":{}},
+            {"Good vs. evil":{}},
+            {"War":{}}]
